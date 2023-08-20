c47082d1c556a6495570c93357fc01a9b02f2b9c
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2023-08-20 11:32:15 +0900

    履歴テーブルで使用中のマシンは削除できないようにする

--- php/content/item_del_done.php
+++ php/content/item_del_done.php
@@ -6,10 +6,35 @@
     require_once(dirname(__FILE__).'/./common/Encode.php');
 
 
-    $result = "削除できませんでした。";
+    //削除キーワードが正しいか?
+    {
+        if ($_POST['key_word'] == "delete") { //削除用キーワード[delete]
+            $deleteFlag = true;
+        }
+        else {
+            $deleteFlag = false;
+            $result = "delete 文字が間違っています。";
+        }    
+    }
 
-    if ($_POST['key_word'] == "delete") { //削除用キーワード[delete]
-        
+    //履歴テーブルで使用されていないか?
+    if ($deleteFlag == true) {
+
+        //DB TABLEから読み出し
+        $where = "item_idx = ".e($_POST['item_idx']);
+        $tblName = "history_tbl";
+        $ret = readTbl($tblName, $where, NULL, NULL, NULL);
+        if ($ret != FALSE) {
+            $deleteFlag = false;
+            $result = "削除できません。使用中のマシンです。";
+        } 
+        else {
+            $deleteFlag = true;
+        }
+    }
+
+    //DBから削除
+    if ($deleteFlag == true) {
         //DB TABLEの要素名リスト
         $paramKeyName = ['item_idx'];
         $paramKeyValue = [];
@@ -23,6 +48,8 @@
         $tblName = "item_tbl";
         if (deleteTbl($tblName, $paramKeyValue) == TRUE) {
             $result = "削除しました。";
+        } else {
+            $result = "削除できませんでした。";
         }
     }
 ?>
@@ -42,7 +69,7 @@
     <script>
         setTimeout(function() {
             window.location.href = "item_list.php";
-        }, 2*1000);
+        }, 200*1000);
     </script>
 
     <?php include(dirname(__FILE__).'/./header/bulma_burger.js'); ?>
